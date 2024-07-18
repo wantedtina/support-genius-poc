7d5edd87d8dc79e54f45e6c850feb2e86a8f86e6
wantedtina <wantedtina@gmail.com>
2024-07-18 16:14:50 +0800

    Use faiss instead of chroma

--- app/knowledge_indexer.py
+++ app/knowledge_indexer.py
@@ -1,30 +1,29 @@
-import json
 import os
-import chromadb
-from chromadb.config import Settings
-from chromadb.utils import embedding_functions
-from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 from langchain_community.document_loaders import PyPDFLoader
-# from langchain_community.document_loaders import BSHTMLLoader
-# from langchain_community.document_loaders import JSONLoader
-# from langchain_community.document_loaders import UnstructuredHTMLLoader
+from langchain_community.document_loaders import BSHTMLLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 import uuid
-from config import Config
 
 class KnowledgeIndexer:
     def __init__(self, folder_path):
         self.folder_path = folder_path
-        # self.data = self.load_pdf()
-        self.data = []
-        self.load_pdf(self.data)
-        # self.load_html(self.data)
-        # self.load_json(self.data)
-        chroma_client = chromadb.PersistentClient(path=folder_path)
-        embedding_function = OpenAIEmbeddingFunction(api_key=Config.OPENAI_API_KEY, api_base=Config.OPENAI_BASE_URL, model_name=Config.EMBED_MODEL)
-        self.collection = chroma_client.get_or_create_collection(name='aa_content', embedding_function=embedding_function)
-        self.populate_collection()
+        self.data = self.load_pdf()
+        embeddings = OpenAIEmbeddings()
+        # embeddings = OllamaEmbeddings(model="llama3")
+        if os.path.exists("faiss_index"):
+            print(f"Loading index from")
+            self.collection = FAISS.load_local(
+                folder_path="faiss_index", 
+                embeddings=embeddings, 
+                allow_dangerous_deserialization=True)
+        else:
+            print(f"Creating a new faiss db")
+            self.collection = FAISS.from_documents(self.data[0], embeddings)
+            self.collection.save_local("faiss_index")
 
-    def load_pdf(self, combined_data):
+    def load_pdf(self):
         combined_data = []
         for filename in os.listdir(self.folder_path):
             if filename.endswith('.pdf'):
@@ -34,27 +33,17 @@ class KnowledgeIndexer:
                     pages = loader.load_and_split()
                     combined_data.append(pages)
         return combined_data
-    
-    # def load_html(self, combined_data):
-    #     # combined_data = []
-    #     for filename in os.listdir(self.folder_path):
-    #         if filename.endswith('.mhtml'):
-    #             file_path = os.path.join(self.folder_path, filename)
-    #             with open(file_path, 'r') as file:
-    #                 loader = UnstructuredHTMLLoader(file_path)
-    #                 data = loader.load()
-    #                 combined_data.append(data)
-    #     return combined_data
 
-    # def load_json(self, combined_data):
-    #     # combined_data = []
-    #     for filename in os.listdir(self.folder_path):
-    #         if filename.endswith('.json'):
-    #             file_path = os.path.join(self.folder_path, filename)
-    #             with open(file_path, 'r') as file:
-    #                 data = json.load(file)
-    #                 combined_data.append(data)
-    #     return combined_data
+    def load_html(self):
+        combined_data = []
+        for filename in os.listdir(self.folder_path):
+            if filename.endswith('.html'):
+                file_path = os.path.join(self.folder_path, filename)
+                with open(file_path, 'r') as file:
+                    loader = BSHTMLLoader(file_path)
+                    data = loader.load()
+                    combined_data.append(data)
+        return combined_data
 
     def populate_collection(self):
         for doc in self.data:
@@ -67,5 +56,5 @@ class KnowledgeIndexer:
                 )
 
     def search(self, query, top_k=3):
-        results = self.collection.query(query_texts=[query], n_results=top_k)
-        return results["documents"][0]
+        results = self.collection.similarity_search(query=query, k=top_k)
+        return results[0].page_content
